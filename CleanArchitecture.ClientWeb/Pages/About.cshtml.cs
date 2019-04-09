68b971d9f462c651b505b7dbeb357604e2478ca0
Brandon Tillman <brandon@brandontillman.com>
2019-04-09 02:29:51 -0700

    Enhanced About
    Enhanced About

--- CleanArchitecture.ClientWeb/Pages/About.cshtml.cs
+++ CleanArchitecture.ClientWeb/Pages/About.cshtml.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Reflection;
+using System.Runtime.Versioning;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CleanArchitecture.ClientWeb.Pages
 {
     public class AboutModel : PageModel
     {
+        public string VersionInfo { get; set; }
+
         public void OnGet()
         {
+            var framework = Assembly
+                .GetEntryAssembly()?
+                .GetCustomAttribute<TargetFrameworkAttribute>()?
+                .FrameworkName;
 
+            VersionInfo = framework;
         }
     }
 }

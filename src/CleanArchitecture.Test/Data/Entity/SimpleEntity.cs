002af6da1e03ba21ced80621f159c6f99d60efb1
Mehmet Bakirci <m.bakirci@outlook.com>
2019-06-24 14:23:25 +0200

    Added generic repository tests
    Added a Test Project and started with Xunit and Moq4

---
+++ src/CleanArchitecture.Test/Data/Entity/SimpleEntity.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Core.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Test.Data.Entity
+{
+    public class SimpleEntity : IEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}

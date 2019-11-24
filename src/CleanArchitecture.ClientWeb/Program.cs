59231a138cdf6ee3b83a79a0d086f79aad15cd87
Brandon Tillman <brandon@brandontillman.com>
2019-11-23 20:50:18 -0700

    Updated application to use .NET Core 3.1
    Application code now runs .NET Core 3.1-preview3

--- src/CleanArchitecture.ClientWeb/Program.cs
+++ src/CleanArchitecture.ClientWeb/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using CleanArchitecture.Infrastructure.Database;
-using Microsoft.AspNetCore;
+﻿using CleanArchitecture.Infrastructure.Database;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace CleanArchitecture.ClientWeb
 {
@@ -20,40 +14,43 @@ namespace CleanArchitecture.ClientWeb
             var logger =
                 NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
 
-            IWebHost host = null;
-
             try
             {
                 logger.Debug("Starting up...");
-                host = BuildWebHost(args);
+                IHostBuilder builder = CreateHostBuilder(args);
+                IHost host = builder.Build();
+
+                // Seed the Database
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    try
+                    {
+                        var context = services.GetRequiredService<ApplicationDbContext>();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                        ApplicationDbInitializer.Initialize(context);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex, "An error occurred while seeding the database.");
+                    }
+                }
+
+                host.Run();
             }
             catch (Exception ex)
             {
                 logger.Error(ex, "Program could not start due to error.");
                 throw;
             }
-
-            // Seed the Database
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationDbContext>();
-                    ApplicationDbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    logger.Error(ex, "An error occurred while seeding the database.");
-                }
-            }
-
-            host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- src/CleanArchitecture.ClientWeb/Startup.cs
+++ src/CleanArchitecture.ClientWeb/Startup.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using CleanArchitecture.Core.Data;
 using CleanArchitecture.Core.Data.Entity;
 using CleanArchitecture.Core.Logging;
+using CleanArchitecture.Core.Mapping;
 using CleanArchitecture.Core.Service;
 using CleanArchitecture.Infrastructure.Database;
 using CleanArchitecture.Infrastructure.Database.Repository;
@@ -12,6 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CleanArchitecture.ClientWeb
 {
@@ -40,19 +42,17 @@ namespace CleanArchitecture.ClientWeb
             services.AddScoped<IArticleCategoryService, ArticleCategoryService>();
 
             // Mappers
-            services.AddAutoMapper();
+            services.AddAutoMapper(typeof(MappingProfile));
 
             // Logging
             services.AddScoped(typeof(ILogger<>), typeof(NLogLogger<>));
 
             // Framework
-            services
-                .AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -66,7 +66,12 @@ namespace CleanArchitecture.ClientWeb
 
             app.UseStaticFiles();
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
         }
     }
 }
